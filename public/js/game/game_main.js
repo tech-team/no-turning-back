f623fe399bc955f7514ffbd6a5aafcc70c9ea400
Abashinos <abashinos@gmail.com>
2014-02-22 00:51:16 +0400

    chessboard (sucks)

--- public/js/game/game_main.js
+++ public/js/game/game_main.js
@@ -20,7 +20,15 @@ function(Player) {
 		},
 
 		render: function() {
-			this.canvas.fillRect(0, 0, 50, 50);
+			this.canvas.fillRect(50, 50, 160, 160);
+            
+            for (i = 0; i < 8; i += 2)
+                for (j = 0; j < 8; j += 2) {
+                    this.canvas.clearRect(50 + i * 20, 50 + j * 20, 20, 20);
+                    this.canvas.clearRect(50 + (i + 1) * 20, 50 + (j + 1) * 20, 20, 20);
+                }
+            
+
 		}
 	};
 
