bd7840ac68fa2301272f9e051083767b815965b1
Abashinos <abashinos@gmail.com>
2014-02-22 11:07:50 +0400

    checkboard fix

--- public/js/game/game_main.js
+++ public/js/game/game_main.js
@@ -5,9 +5,16 @@ function(Player) {
 	var Game = {
 		FPS: 60,
 		canvas: null,
+		context: null,
+		width: 300,
+		height: 300,
 
 		run: function (_canvas) {
-			this.canvas = _canvas.getContext("2d");
+			this.canvas = _canvas;
+			this.canvas.width = this.width;
+			this.canvas.height = this.height;
+			this.context = _canvas.getContext("2d");
+			
 			var self = this;
 			setInterval(function() {
 				self.update();
@@ -20,12 +27,12 @@ function(Player) {
 		},
 
 		render: function() {
-			this.canvas.fillRect(50, 50, 160, 160);
+			this.context.fillRect(50, 50, 160, 160);
             
             for (i = 0; i < 8; i += 2)
                 for (j = 0; j < 8; j += 2) {
-                    this.canvas.clearRect(50 + i * 20, 50 + j * 20, 20, 20);
-                    this.canvas.clearRect(50 + (i + 1) * 20, 50 + (j + 1) * 20, 20, 20);
+                    this.context.clearRect(50 + i * 20, 50 + j * 20, 20, 20);
+                    this.context.clearRect(50 + (i + 1) * 20, 50 + (j + 1) * 20, 20, 20);
                 }
             
 
