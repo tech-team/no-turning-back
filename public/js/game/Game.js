c3666f0000416fef899a89f55661c3c05900f296
Abashinos <abashinos@gmail.com>
2014-02-27 23:18:53 +0400

    FPS

--- public/js/game/Game.js
+++ public/js/game/Game.js
@@ -5,7 +5,7 @@ define([
 function(Class, createjs) {
 	var Game = Class.$extend({
 		__init__: function(canvas) {
-			this.FPS = 30;
+			this.FPS = 60;
 			this.canvas = canvas;
 			this.stage = new createjs.Stage(this.canvas);
 			this.ticker = createjs.Ticker;
@@ -24,22 +24,32 @@ function(Class, createjs) {
 		},
 
 		circle: null,
+        output: null,
 
 		update: function(event) {
 
-			this.circle.x += event.delta / 1000 * 100;
-			
+			this.circle.x += event.delta / 1000 * 300;
 
-			this.stage.update(event);
+            if (this.circle.x + 50 > this.stage.canvas.width) {
+                this.circle.x = 50;
+            }
+
+            this.output.text = "FPS = " + Math.round(createjs.Ticker.getMeasuredFPS());
+
+            this.stage.update(event);
 		},
 
 		test: function() {
 			this.circle = new createjs.Shape();
-			this.circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+			this.circle.graphics.beginFill("blue").drawCircle(100, 100, 50);
 
-			this.circle.x = 100;
-			this.circle.y = 100;
+            this.output = this.stage.addChild(new createjs.Text("","bold 16px Verdana", "#000"))
+            this.output.lineHeight = 15;
+            this.output.textBaseline = "top";
+            this.output.x = 100;
+            this.output.y = 15;
 			this.stage.addChild(this.circle);
+
 		}
 	});
 
