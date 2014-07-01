2c44c04c9f6f1b3180c54118fec0ea50c495b851
alekseyl1992 <alekseyl@list.ru>
2014-07-01 18:59:59 +0400

    Closes #76

--- deploy-db.bat
+++ deploy-db.bat
@@ -2,4 +2,4 @@ mongo localhost:27017/test mongodb-create.js
 
 set pathname=.\public\levels
 
-for %%file in (%pathname%\*) do mongoimport --db NTBdb --collection levels --file %%file
+for %%f in (%pathname%\*) do mongoimport --db NTBdb --collection levels --file "%%f"
